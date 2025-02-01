5474878b005dac3b415a58bc178dbb212f2efbc7
Belthronding01 <denizakin113@gmail.com>
2025-02-01 16:16:22 +0300

    Added text_node_to_html_node function to convert a TextNode to a LeafNode

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,5 +1,6 @@
 import unittest
-from textnode import TextNode, TextType
+from textnode import *
+from leafnode import LeafNode
 
 class TestTextNode(unittest.TestCase):
 	def test_eq(self):
@@ -22,6 +23,47 @@ class TestTextNode(unittest.TestCase):
 		node = TextNode("asdssd", TextType.CODE, "https://boot.dev")
 		node2 = TextNode("asds", TextType.CODE, "https://boot.dev")
 		self.assertNotEqual(node.text, node2.text)
-
+	def test_text_node_to_html_node(self):
+		# Test plain text
+		node = TextNode("Hello, world!", TextType.TEXT)
+		html_node = text_node_to_html_node(node)
+		assert html_node.tag is None
+		assert html_node.value == "Hello, world!"
+		assert html_node.props is None
+	def test_text_node_to_html_node_case_bold(self):
+		# Test bold text
+		node = TextNode("Bold text", TextType.BOLD)
+		html_node = text_node_to_html_node(node)
+		assert html_node.tag == "b"
+		assert html_node.value == "Bold text"
+		assert html_node.props is None
+	def test_text_node_to_html_node_case_italic(self):
+		# Test italic text
+		node = TextNode("Italic text", TextType.ITALIC)
+		html_node = text_node_to_html_node(node)
+		assert html_node.tag == "i"
+		assert html_node.value == "Italic text"
+		assert html_node.props is None
+	def test_text_node_to_html_node_case_code(self):
+		# Test code text
+		node = TextNode("Code", TextType.CODE)
+		html_node = text_node_to_html_node(node)
+		assert html_node.tag == "code"
+		assert html_node.value == "Code"
+		assert html_node.props is None
+	def test_text_node_to_html_node_case_link(self):
+		# Test link
+		node = TextNode("Click", TextType.LINK, "https://boot.dev")
+		html_node = text_node_to_html_node(node)
+		assert html_node.tag == "a"
+		assert html_node.value == "Click"
+		assert html_node.props == {"href":"https://boot.dev"}
+	def test_text_node_to_html_node_case_image(self):
+		# Test image
+		node = TextNode("Image text", TextType.IMAGE, "https://boot.dev/image.png")
+		html_node = text_node_to_html_node(node)
+		assert html_node.tag == "img"
+		assert html_node.value == ""
+		assert html_node.props == {"src": "https://boot.dev/image.png", "alt":"Image text"}
 if __name__ == "__main__":
 	unittest.main()
--- src/textnode.py
+++ src/textnode.py
@@ -1,4 +1,25 @@
 from enum import Enum
+from leafnode import LeafNode
+
+__all__ = ["TextNode", "TextType", "text_node_to_html_node"]
+
+def text_node_to_html_node(text_node):
+		match (text_node.text_type):
+			case TextType.TEXT:
+				return LeafNode(None, text_node.text, None)
+			case TextType.BOLD:
+				return LeafNode("b", text_node.text, None)
+			case TextType.ITALIC:
+				return LeafNode("i", text_node.text, None)
+			case TextType.CODE:
+				return LeafNode("code", text_node.text, None)
+			case TextType.LINK:
+				return LeafNode("a", text_node.text, {"href":text_node.url})
+			case TextType.IMAGE:
+				node = LeafNode("img", "", {"src": text_node.url, "alt": text_node.text})
+				return node
+			case _:
+				raise ValueError(f"Invalid text type {text_node.text_type}")
 
 class TextType(Enum):
 	BOLD = "bold"
@@ -20,5 +41,3 @@ class TextNode():
 		return False
 	def __repr__(self):
 		return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
-	
-
